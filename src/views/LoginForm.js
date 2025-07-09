cd351503f9a6caa375f42e9c939cbcd305b74fa6
Guntur Dwiyatmoko <guntur@solusiti.com>
2025-07-09 22:23:10 +0700

    reset password added

--- src/views/LoginForm.js
+++ src/views/LoginForm.js
@@ -193,65 +193,6 @@ const LoginForm = {
   },
 }
 
-// const ForgotPasswordForm = {
-//   email: "",
-//   success: "",
-//   error: "",
-//   loading: false,
-
-//   view() {
-//     const lang = localStorage.getItem("lang") || "en"
-//     const t = i18n[lang]
-//     document.documentElement.setAttribute("data-theme", localStorage.getItem("theme") || "auto")
-
-//     return m("main.container", [
-//       m("article", [
-//         m("hgroup", [
-//           m("h1", t.heading),
-//           m("h2", t.subheading),
-//         ]),
-
-//         ForgotPasswordForm.success && m("p", { style: "color: green" }, t.success),
-//         ForgotPasswordForm.error && m("p", { style: "color: red" }, t.error),
-
-//         m("form", {
-//           onsubmit: async (e) => {
-//             e.preventDefault()
-//             ForgotPasswordForm.success = ForgotPasswordForm.error = ""
-//             ForgotPasswordForm.loading = true
-
-//             try {
-//               await m.request({
-//                 method: "POST",
-//                 url: "/api/request-password-reset",
-//                 body: { email: ForgotPasswordForm.email },
-//               })
-//               ForgotPasswordForm.success = true
-//             } catch (err) {
-//               ForgotPasswordForm.error = true
-//             }
-
-//             ForgotPasswordForm.loading = false
-//           }
-//         }, [
-//           m("label", { for: "email" }, t.email),
-//           m("input", {
-//             id: "email",
-//             type: "email",
-//             value: ForgotPasswordForm.email,
-//             oninput: e => ForgotPasswordForm.email = e.target.value,
-//           }),
-//           m("button", {
-//             type: "submit",
-//             disabled: !ForgotPasswordForm.email || ForgotPasswordForm.loading,
-//             'aria-busy': ForgotPasswordForm.loading ? "true" : null,
-//           }, t.button),
-//         ]),
-//       ]),
-//     ])
-//   }
-// }
-
-
+  
 
 export default LoginForm
